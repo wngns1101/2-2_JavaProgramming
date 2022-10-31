a96c379f8e0664d55924cd3f110f8efb59599395
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 <wngns0812@gmail.com>
2022-10-31 23:22:24 +0900

    access db

---
+++ JDBC_Source/src/DB_MAN.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+import java.io.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author inhatc
+ */
+public class DB_MAN {
+
+String strDriver = "sun.jdbc.odbc.JdbcOdbcDriver";
+String strURL = "jdbc:odbc:Automobile64";
+
+Connection DB_con;
+Statement DB_stmt;
+ResultSet DB_rs;
+
+public void dbOpen() throws IOException{
+    try{
+        Class.forName(strDriver);
+        DB_con = DriverManager.getConnection(strURL);
+        DB_stmt = DB_con.createStatement();
+    }catch(Exception e){
+        System.out.println("SQLException: " + e.getMessage());
+    }
+}    
+public void dbClose() throws IOException{
+    try{
+        DB_stmt.close();
+        DB_con.close();
+    }catch(Exception e){
+        System.out.println("SQLException: " + e.getMessage());
+    }
+}    
+
+}
